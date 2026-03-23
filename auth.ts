764d841ad9e61b185109eca5467912dbb1af628c
Admir Skomorac <admir.skomorac@outlook.com>
2026-03-23 11:15:50 +0100

    security: fix vulnerabilities and migrate to bcryptjs

--- auth.ts
+++ auth.ts
@@ -1,10 +1,11 @@
+
 import NextAuth from 'next-auth';
 import Credentials from 'next-auth/providers/credentials';
 import { authConfig } from './auth.config';
 import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import type { User } from '@/app/lib/definitions';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 
 async function getUser(email: string): Promise<User | undefined> {
   try {
